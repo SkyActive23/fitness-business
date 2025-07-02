58a745d9c6be931daf8cabc3774f170acdd2e34a
Skyler <skyrj50031@gccaz.edu>
2025-07-01 22:41:49 -0700

    Delete unused use effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import Carousel from "@/app/Components/Carousel/Carousel";
 import Contact from "@/app/Components/Contact/Contact";
